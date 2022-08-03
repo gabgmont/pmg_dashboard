0b6babfbce9ec5c2570f8c5a1652a7dfdd7b24fe
gustavoglorde <40869698+gustavoglorde@users.noreply.github.com>
2022-08-02 22:45:35 -0300

    FORMULARIO 1/ PAGEVIEW

--- module_registration/lib/pages/registration/registration_page.dart
+++ module_registration/lib/pages/registration/registration_page.dart
@@ -4,7 +4,15 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:module_commons/export_bloc.dart';
+import 'package:module_design/assets/pmg_icons.dart';
 import 'package:module_design/assets/pmg_images.dart';
+import 'package:module_design/components/button/pmg_button.dart';
+import 'package:module_design/components/button/pmg_button_config.dart';
+import 'package:module_design/components/drop_down/pmg_drop_down.dart';
+import 'package:module_design/components/drop_down/pmg_drop_down_item.dart';
+import 'package:module_design/components/text_field/pmg_text_field.dart';
+import 'package:module_design/styles/pmg_colors.dart';
+import 'package:module_registration/pages/registration/widgets/registration_location.dart';
 
 class RegistrainPage extends StatefulWidget {
   const RegistrainPage({Key? key}) : super(key: key);
@@ -14,24 +22,69 @@ class RegistrainPage extends StatefulWidget {
 }
 
 class _RegistrainPageState extends State<RegistrainPage> {
+  int? dropDownValue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            child: Row(
-              children: [
-                PmgImage(image: PmgImages.pmg_logo),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                SizedBox(
+                  width: 120,
+                  child: PmgImage(image: PmgImages.pmg_logo),
+                ),
+                SizedBox(
+                  width: 15.77,
+                ),
                 Text(
                   'Contrate seu seguro',
-                  style: TextStyle(fontSize: 32, fontStyle: FontStyle.normal),
+                  style: TextStyle(
+                      fontSize: 32,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.bold,
+                      color: PmgColors.neutralDark),
                 ),
               ],
             ),
-            width: MediaQuery.of(context).size.width * 0.88,
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(top: 68.0, left: 25),
+              child: Row(
+                children: [
+                  const Text(
+                    'Nos diga um pouco mais sobre sua locação',
+                    style: TextStyle(
+                        fontSize: 48,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        color: PmgColors.neutralDark),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width * 0.20,
+                        right: 26),
+                    child: PmgButton(
+                      onTap: () {},
+                      buttonType: PmgButtonType.text,
+                      rightIcon: PmgIcons.arrow_right,
+                      buttonSize: PmgButtonSize.extraLarge,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            // formulario
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: PageView(
+                children: [RegistrationLocation()],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ module_registration/lib/pages/registration/widgets/registration_location.dart
@@ -0,0 +1,287 @@
+import 'package:flutter/material.dart';
+import 'package:module_design/components/drop_down/pmg_drop_down.dart';
+import 'package:module_design/components/drop_down/pmg_drop_down_item.dart';
+import 'package:module_design/components/text_field/pmg_text_field.dart';
+import 'package:module_design/styles/pmg_colors.dart';
+
+class RegistrationLocation extends StatefulWidget {
+  const RegistrationLocation({Key? key}) : super(key: key);
+
+  @override
+  State<RegistrationLocation> createState() => _RegistrationLocationState();
+}
+
+class _RegistrationLocationState extends State<RegistrationLocation> {
+  @override
+  Widget build(BuildContext context) {
+    int? dropDownValue;
+    return Padding(
+      padding: const EdgeInsets.only(top: 64),
+      child: SizedBox(
+        // tamanho
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height * 0.60,
+        // formulario
+        child: Row(
+          children: [
+            // formulario 1
+            SizedBox(
+              width: 250,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text(
+                      'Dados do imovel',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.bold,
+                          color: PmgColors.neutralDark),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    PmgDropDown(
+                      dropDownValue: dropDownValue,
+                      clearOption: true,
+                      itens: [
+                        PmgDropDownItem(
+                            value: 1, content: const Text('Opcao 1')),
+                        PmgDropDownItem(
+                            value: 2, content: const Text('Opcao 2'))
+                      ],
+                      onItemSelected: (value) =>
+                          setState(() => dropDownValue = value),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    PmgTextField(
+                      controller: TextEditingController(),
+                      label: 'nome',
+                      hint: 'Fulano',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    PmgTextField(
+                      controller: TextEditingController(),
+                      label: 'nome',
+                      hint: 'Fulano',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    PmgTextField(
+                      controller: TextEditingController(),
+                      label: 'nome',
+                      hint: 'Fulano',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    PmgTextField(
+                      controller: TextEditingController(),
+                      label: 'nome',
+                      hint: 'Fulano',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    PmgTextField(
+                      controller: TextEditingController(),
+                      label: 'nome',
+                      hint: 'Fulano',
+                      errorMessage: 'digita correto',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    PmgTextField(
+                      controller: TextEditingController(),
+                      label: 'nome',
+                      hint: 'Fulano',
+                      errorMessage: 'digita correto',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    PmgTextField(
+                      controller: TextEditingController(),
+                      label: 'nome',
+                      hint: 'Fulano',
+                      errorMessage: 'digita correto',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    PmgTextField(
+                      controller: TextEditingController(),
+                      label: 'nome',
+                      hint: 'Fulano',
+                      errorMessage: 'digita correto',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            // formulario 2
+            SizedBox(
+              width: 250,
+              child: Column(
+                children: [
+                  Text(
+                    'Dados da imobiliária',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        color: PmgColors.neutralDark),
+                  ),
+                  SizedBox(
+                    height: 13,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            // formulario 3
+            SizedBox(
+              width: 250,
+              child: Column(
+                children: [
+                  Text(
+                    'Gastos do imóvel',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        color: PmgColors.neutralDark),
+                  ),
+                  SizedBox(
+                    height: 13,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            //formulario 4
+            SizedBox(
+              width: 250,
+              child: Column(
+                children: [
+                  Text(
+                    'Sobre a locação',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        color: PmgColors.neutralDark),
+                  ),
+                  SizedBox(
+                    height: 13,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  PmgTextField(
+                    controller: TextEditingController(),
+                    label: 'nome',
+                    hint: 'Fulano',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
